f5a3b5f4cd97d10720f5f635629b6b4866f17fdc
Robert <robertbeaudenon@gmail.com>
2020-09-06 22:15:59 -0400

    Generated token when user registers

--- backend/flightscheduler/flightscheduler/users/models.py
+++ backend/flightscheduler/flightscheduler/users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class CustomUser(AbstractUser):
@@ -11,3 +15,9 @@ class CustomUser(AbstractUser):
 
     def get_username(self):
         return self.email
+
+    # Create token for user who just registered
+    @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+    def create_auth_token(sender, instance=None, created=False, **kwargs):
+        if created:
+            Token.objects.create(user=instance)
